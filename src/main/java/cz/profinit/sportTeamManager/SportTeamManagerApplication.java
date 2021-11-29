525639db3e23edd84530b905319a6b03f9d83602
jjansky <jjansky@profinit.eu>
2021-11-29 15:59:37 +0100

    Maven project init

---
+++ src/main/java/cz/profinit/sportTeamManager/SportTeamManagerApplication.java
@@ -0,0 +1,13 @@
+package cz.profinit.sportTeamManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTeamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTeamManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/profinit/sportTeamManager/SportTeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.profinit.sportTeamManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportTeamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
