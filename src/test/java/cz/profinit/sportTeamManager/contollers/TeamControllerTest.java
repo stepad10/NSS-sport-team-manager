ddc8f189432c65b3dd1a04820c2a4d7a1722c578
jjansky <jjansky@profinit.eu>
2021-12-22 17:12:40 +0100

    Team authorization

--- src/test/java/cz/profinit/sportTeamManager/contollers/TeamControllerTest.java
+++ src/test/java/cz/profinit/sportTeamManager/contollers/TeamControllerTest.java
@@ -45,6 +45,7 @@ import static org.hamcrest.Matchers.containsString;
 @SpringBootTest(classes = SportTeamManagerApplicationTests.class)
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @WebAppConfiguration
+@WebMvcTest
 @AutoConfigureMockMvc
 @ActiveProfiles({"stub_repository","stub_services","webTest","test","authentication"})
 public class TeamControllerTest {
