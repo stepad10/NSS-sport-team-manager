5d8ca92435fffb198351d7258b9684eb8ae07752
PROFINIT.LAN\dstepanek <dstepanek@profinit.eu>
2022-01-04 15:55:41 +0100

    deleted empty files

--- src/main/java/cz/profinit/sportTeamManager/repositories/team/SubgroupRepository.java
+++
@@ -1,5 +0,0 @@
-package cz.profinit.sportTeamManager.repositories.team;
-
-
-public interface SubgroupRepository {
-}
--- src/test/java/cz/profinit/sportTeamManager/stubs/stubRepositories/team/StubSubgroupRepository.java
+++
@@ -1,6 +0,0 @@
-package cz.profinit.sportTeamManager.stubs.stubRepositories.team;
-
-import cz.profinit.sportTeamManager.repositories.team.SubgroupRepository;
-
-public class StubSubgroupRepository implements SubgroupRepository {
-}
