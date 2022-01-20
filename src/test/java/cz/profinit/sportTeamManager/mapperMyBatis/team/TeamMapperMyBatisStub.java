b5c6acb3dd6d2ebfb56ff326d2a73647fda932eb
PROFINIT.LAN\dstepanek <dstepanek@profinit.eu>
2022-01-20 09:50:01 +0100

    MyBatis methods return values redone

--- src/test/java/cz/profinit/sportTeamManager/mapperMyBatis/team/TeamMapperMyBatisStub.java
+++ src/test/java/cz/profinit/sportTeamManager/mapperMyBatis/team/TeamMapperMyBatisStub.java
@@ -1,35 +1,33 @@
 package cz.profinit.sportTeamManager.mapperMyBatis.team;
 
+import cz.profinit.sportTeamManager.exceptions.EntityNotFoundException;
 import cz.profinit.sportTeamManager.model.team.Team;
 import cz.profinit.sportTeamManager.model.user.RegisteredUser;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-@Component
+@Repository("team_stub")
 public class TeamMapperMyBatisStub implements TeamMapperMyBatis {
 
     private final Logger logger = Logger.getLogger(String.valueOf(getClass()));
 
     @Override
-    public Team insertTeam(Team team) {
+    public void insertTeam(Team team) {
         logger.info("STUB: Inserting team to database!");
-        return team;
+        team.setEntityId(10L);
     }
 
     @Override
-    public Team updateTeam(Team team) {
+    public void updateTeam(Team team) {
         logger.info("STUB: Updating team in database!");
-        return team;
     }
 
     @Override
-    public Team deleteTeamById(Long id) {
+    public void deleteTeamById(Long id) {
         logger.info("STUB: deleting team in database!");
-        if (id == 0L) return null;
-        return new Team("Profinit", "Famfrpal", new ArrayList<>(), new RegisteredUser());
     }
 
     @Override
