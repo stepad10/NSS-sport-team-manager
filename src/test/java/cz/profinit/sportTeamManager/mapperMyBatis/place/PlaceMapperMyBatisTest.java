7abe841dd5ec7a472f95655a913ccd6c5b2d4b51
PROFINIT.LAN\dstepanek <dstepanek@profinit.eu>
2022-01-24 10:49:49 +0100

    init configuration

--- src/test/java/cz/profinit/sportTeamManager/mapperMyBatis/place/PlaceMapperMyBatisTest.java
+++ src/test/java/cz/profinit/sportTeamManager/mapperMyBatis/place/PlaceMapperMyBatisTest.java
@@ -1,4 +1,20 @@
 package cz.profinit.sportTeamManager.mapperMyBatis.place;
 
+import cz.profinit.sportTeamManager.configuration.MyBatisConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@ContextConfiguration(classes = MyBatisConfiguration.class)
+@TestPropertySource("/test.properties")
 public class PlaceMapperMyBatisTest {
+
+    @Test
+    public void test(){}
 }
