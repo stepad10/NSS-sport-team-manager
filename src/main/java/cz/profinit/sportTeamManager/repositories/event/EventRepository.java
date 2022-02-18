0ea1a5753060523cce521d86f615be025a3e67aa
PROFINIT.LAN\dstepanek <dstepanek@profinit.eu>
2022-02-18 11:25:01 +0100

    added javaDoc

--- src/main/java/cz/profinit/sportTeamManager/repositories/event/EventRepository.java
+++ src/main/java/cz/profinit/sportTeamManager/repositories/event/EventRepository.java
@@ -16,7 +16,26 @@ import cz.profinit.sportTeamManager.model.event.Event;
  */
 public interface EventRepository {
 
+    /**
+     * TODO return not needed, but hard to change
+     * @param event to insert
+     * @return inserted event
+     * @throws EntityNotFoundException if Event could not be found
+     */
     Event insertEvent(Event event) throws EntityNotFoundException;
+
+    /**
+     *
+     * @param id to find Event by
+     * @return found Event
+     * @throws EntityNotFoundException if Event could not be found
+     */
     Event findEventById(Long id) throws EntityNotFoundException;
+
+    /**
+     *
+     * @param event to update
+     * @throws EntityNotFoundException if Event could not be found
+     */
     void updateEvent(Event event) throws EntityNotFoundException;
 }
