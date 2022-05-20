d373ee1e8b7f7b57a5bc5f295ff753d2034ed567
David Štěpánek <stepad10@fel.cvut.cz>
2022-05-20 08:32:50 +0200

    init user

---
+++ FE/sport-team-manager/src/app/shared/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  name: string;
+  surname: string;
+  email: string;
+
+  constructor(name: string, surname: string, email: string) {
+    this.surname = surname;
+    this.name = name;
+    this.email = email;
+  }
+}
